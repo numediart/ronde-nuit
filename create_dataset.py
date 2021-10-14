cf32882bc04e1bf3662ea7bea6f9f0f6582ec98d
Pierre Marighetto <pierre.marighetto@gmail.com>
2021-10-14 13:07:08 +0200

    Clean up files

--- create_dataset.py
+++ create_dataset.py
@@ -2,7 +2,7 @@
 '''
 import argparse
 
-from src.dataset import create_splits
+from src.dataset import create_dataset
 
 
 def parse_args():
@@ -14,12 +14,18 @@ def parse_args():
         dictionary of input arguments
     '''
     parser = argparse.ArgumentParser(
-        description='Converts a JSON file into a CSV file.')
-    parser.add_argument('path', type=str,
-                        help='path to JSON file.')
-    parser.add_argument('-c', '--csv', type=str,
-                        default='output.csv',
-                        help='CSV output file to have conversion.')
+        description='Creates a dataset for retraining based on folder of CSV files.')
+    parser.add_argument('infolder', type=str,
+                        help='path to folder containing annotated CSVs. CSVs should have a message and label column.')
+    parser.add_argument('-o', '--outfolder', type=str,
+                        default='data/sorted/',
+                        help='path to folder that will contain the CSVs.')
+    parser.add_argument('-r', '--ratio', type=float,
+                        default=0.3,
+                        help='percentage of data that will be used for testing.')
+    parser.add_argument('-v', '--verbose', type=bool,
+                        default=True,
+                        help='option to show statistics on data when creating.')
     opt = parser.parse_args()
 
     return opt
@@ -28,3 +34,5 @@ def parse_args():
 if __name__ == '__main__':
     # Load parameters
     opt = parse_args()
+
+    create_dataset(opt.infolder, opt.outfolder, opt.test_ratio, opt.verbose)
--- src/dataset.py
+++ src/dataset.py
@@ -1,42 +1,95 @@
 import glob
 import os
-from pathlib import Path
 
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
 
-def get_statistics(data):
+def print_ratio(data: pd.DataFrame,
+                label: str) -> None:
+    '''Print ratio of a given label
+
+    Args
+    ----
+    data : pd.DataFrame
+        DataFrame where labels come from
+    label : str
+        label to get ratio of. Can either be 'positive', 'negative' or 'neutral'
+    '''
     print(
-        f"Positive ratio: {len(data.loc[data['label'] == 'positive']) / len(data)}")
-    print(
-        f"Neutral ratio: {len(data.loc[data['label'] == 'neutral']) / len(data)}")
-    print(
-        f"Negative ratio: {len(data.loc[data['label'] == 'negative']) / len(data)}")
+        f"{label} ratio: {len(data.loc[data['label'] == label.lower()]) / len(data)}")
 
 
-def merge_information(path):
+def get_statistics(data: pd.DataFrame,
+                   neutral=True) -> None:
+    '''Print statistics of each label in the dataset.
+
+    Args
+    ----
+    data : pd.DataFrame
+        DataFrame where labels come from
+
+    '''
+    if not neutral:
+        data = data.loc[data['label'] != 'neutral']
+    print_ratio(data, "Positive")
+
+    if neutral:
+        print_ratio(data, "Neutral")
+    print_ratio(data, "Negative")
+
+
+def merge_information(path: str) -> pd.DataFrame:
+    '''Merge information from csv in the same folder.
+
+    Args
+    ----
+    path : str
+        path to the folder containing all the csv 
+
+    Returns
+    -------
+    pd.DataFrame
+        DataFrame containing the messages and their label from each CSV
+    '''
+    # Define lower fonction for an array
     lower = np.vectorize(str.lower)
+
+    # Init columns
     sequences = []
     labels = []
+
+    # CSVs are delimited with semicolumns (;)
+    # CSVs should have message and label columns
     for elem in glob.glob(os.path.join(path, '*.csv')):
         data = pd.read_csv(elem, delimiter=';')
-        data = data.dropna()
-        sequences += data['sequence'].to_list()
+        data = data.dropna()  # Skipped annotation are converted to NaN
+        sequences += data['message'].to_list()
         labels += data['label'].to_list()
 
+    # Create the dataframe
     df = pd.DataFrame()
     df.insert(0, 'label', lower(labels))
     df.insert(0, 'sequence', sequences)
     return df
 
 
-def select_label_and_store(data: pd.DataFrame, label: str, location: str):
-    label_data = data.loc[data['label'] == label]
+def create_csv_splits(data: pd.DataFrame,
+                      path: str,
+                      test_ratio: float = 0.3) -> None:
+    '''Create CSV files for training and testing.
 
-
-def create_csv_splits(data, path, test_ratio=0.3):
+    Args
+    ----
+    data : pd.DataFrame
+        DataFrame containing all the annotated data.
+    path : str
+        path to the folder that will contain train.csv and test.csv
+    test_ratio : float, optional
+        percentage of data that will be used for testing.
+        Default is 0.3.
+    '''
     train, test = train_test_split(data, test_size=test_ratio)
 
     os.makedirs(path, exist_ok=True)
@@ -44,18 +97,27 @@ def create_csv_splits(data, path, test_ratio=0.3):
     test.to_csv(os.path.join(path, 'test.csv'), index=False)
 
 
-def sort_data(inpath, outpath):
-    data = merge_information(inpath)
-    create_csv_splits(data, outpath)
+def create_dataset(infolder: str,
+                   outfolder: str,
+                   test_ratio: float = 0.3,
+                   verbose: bool = True) -> None:
+    '''Creates a dataset of retraining based on annotated CSVs.
 
+    Args
+    ----
+    infolder : str
+        folder containing all annotated CSVs
+    outfolder : str
+        folder that will contain CSVs for training (train.csv and test.csv)
+    test_ratio : float, optional
+        percentage of data that will be used for testing.
+        Default is 0.3.
+    verbose : bool, optional
+        Weither to show additional information while running.
+        Default is True.
+    '''
+    data = merge_information(infolder)
+    if verbose:
+        get_statistics(data)
 
-def create_splits(path):
-    texts = []
-    labels = []
-
-    data = pd.read_csv(path)
-    data.loc[data['label'] != 'neutral']
-    texts = data['sequence'].to_list()
-    labels = [0 if x == 'negative' else 1 for x in data['label'].to_list()]
-
-    return texts, labels
+    create_csv_splits(data, outfolder, test_ratio)
